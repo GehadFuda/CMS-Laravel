d3b6ef08c03c7e5682b206aa8a63c85fb9efc374
Gehad Fuda <gehad.fuda@gmail.com>
2021-09-20 21:44:27 +0200

    Installing our frontend theme

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,166 @@
+<?php
+
+use App\Category;
+use App\Post;
+use App\Tag;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category1 = Category::create([
+            'name' => 'News'
+        ]);
+        
+        $category2 = Category::create([
+            'name' => 'Updates'
+        ]);
+
+        $category3 = Category::create([
+            'name' => 'Design'
+        ]);
+
+        $category4 = Category::create([
+            'name' => 'Marketing'
+        ]);
+
+        $category5 = Category::create([
+            'name' => 'Partnership'
+        ]);
+
+        $category6 = Category::create([
+            'name' => 'Product'
+        ]);
+
+        $category7 = Category::create([
+            'name' => 'Hiring'
+        ]);
+
+        $category8 = Category::create([
+            'name' => 'Offers'
+        ]);
+
+        $post1 = Post::create([
+            'title' => 'We relocated our office to a new designed garage',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'content' => 'Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+            ',
+            'category_id' => $category1->id,
+            'image' => 'posts/1.jpg'
+        ]);
+
+        $post2 = Post::create([
+            'title' => 'Top 5 brilliant content marketing strategies',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'content' => 'Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+            ',
+            'category_id' => $category2->id,
+            'image' => 'posts/2.jpg'
+        ]);
+
+        $post3 = Post::create([
+            'title' => 'Best practices for minimalist design with example',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'content' => 'Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+            ',
+            'category_id' => $category3->id,
+            'image' => 'posts/3.jpg'
+        ]);
+
+        $post4 = Post::create([
+            'title' => 'Congratulate and thank to Maryam for joining our team',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'content' => 'Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+            ',
+            'category_id' => $category4->id,
+            'image' => 'posts/4.jpg'
+        ]);
+
+        $post5 = Post::create([
+            'title' => 'New published books to read by a product designer',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'content' => 'Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+            ',
+            'category_id' => $category5->id,
+            'image' => 'posts/5.jpg'
+        ]);
+
+        $post6 = Post::create([
+            'title' => 'This is why it\'s time to ditch dress codes at work',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'content' => 'Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+            ',
+            'category_id' => $category6->id,
+            'image' => 'posts/6.jpg'
+        ]);
+
+        $tag1 = Tag::create([
+            'name' => 'Record'
+        ]);
+        
+        $tag2 = Tag::create([
+            'name' => 'Progress'
+        ]);
+
+        $tag3 = Tag::create([
+            'name' => 'Customers'
+        ]);
+
+        $tag4 = Tag::create([
+            'name' => 'Freebie'
+        ]);
+
+        $tag5 = Tag::create([
+            'name' => 'Offer'
+        ]);
+
+        $tag6 = Tag::create([
+            'name' => 'Screenshot'
+        ]);
+
+        $tag7 = Tag::create([
+            'name' => 'Milestone'
+        ]);
+
+        $tag8 = Tag::create([
+            'name' => 'Version'
+        ]);
+
+        $tag9 = Tag::create([
+            'name' => 'Design'
+        ]);
+
+        $tag10 = Tag::create([
+            'name' => 'Customers'
+        ]);
+
+        $tag11 = Tag::create([
+            'name' => 'Job'
+        ]);
+
+        $post1->tags()->attach([$tag1->id, $tag2->id]);
+        $post2->tags()->attach([$tag3->id, $tag4->id]);
+        $post3->tags()->attach([$tag5->id, $tag6->id]);
+        $post4->tags()->attach([$tag7->id, $tag8->id]);
+        $post5->tags()->attach([$tag9->id, $tag10->id]);
+        $post6->tags()->attach([$tag11->id, $tag2->id]);
+    }
+}
