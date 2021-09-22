b4fed899cffca529466ae07abffcf7f4e6eaa366
Gehad Fuda <gehad.fuda@gmail.com>
2021-09-23 01:56:53 +0200

    Query scopes

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,17 +12,17 @@ class WelcomeController extends Controller
     public function index()
     {
 
-        $search = request()->query('search');
-        if ($search) {
-            // dd(request()->query('search'));
-            $post = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(3);
-        } else {
-            $post = Post::simplePaginate(3);
-        }
+        // $search = request()->query('search');
+        // if ($search) {
+        //     // dd(request()->query('search'));
+        //     $post = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(3);
+        // } else {
+        //     $post = Post::simplePaginate(3);
+        // }
 
         return view('welcome')
             ->with('categories', Category::all())
             ->with('tags', Tag::all())
-            ->with('posts', $post);
+            ->with('posts', Post::searched()->simplePaginate(3));
     }
 }
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -91,5 +91,9 @@
     <script src="{{ asset('js/page.min.js') }}"></script>
     <script src="{{ asset('js/script.js') }}"></script>
 
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-614bc07042586731"></script>
+
+
   </body>
 </html>
